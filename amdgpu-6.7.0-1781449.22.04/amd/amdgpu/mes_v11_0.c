1772716ffb25b5239b9c72cf18fe2cd0ee74a11a
nimlgen <138685161+nimlgen@users.noreply.github.com>
2024-06-12 19:05:08 +0000

    higher mes timout

--- amdgpu-6.7.0-1781449.22.04/amd/amdgpu/mes_v11_0.c
+++ amdgpu-6.7.0-1781449.22.04/amd/amdgpu/mes_v11_0.c
@@ -108,7 +108,7 @@ static int mes_v11_0_submit_pkt_and_poll_completion(struct amdgpu_mes *mes,
 	struct amdgpu_device *adev = mes->adev;
 	struct amdgpu_ring *ring = &mes->ring;
 	unsigned long flags;
-	signed long timeout = 3000000; /* 3000 ms */
+	signed long timeout = 10000000; /* 10s */
 	u32 fence_offset;
 	u64 fence_gpu_addr;
 	u64 *fence_ptr;
@@ -157,6 +157,9 @@ static int mes_v11_0_submit_pkt_and_poll_completion(struct amdgpu_mes *mes,
 			schedule();
 
 		return -ETIMEDOUT;
+	} else if ((timeout - r) / 1000 > 100) {
+		DRM_WARN("MES msg=%d response took %ld ms\n",
+				 x_pkt->header.opcode, (timeout - r) / 1000);
 	}
 
 	return 0;
